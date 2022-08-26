8c8e7ea0e021ead5ed5e5486fb97e4e00b1b13d2
Michael <mchu4545@gmail.com>
2022-08-26 09:38:55 -0700

    script to execute the ECNE command

---
+++ batchRunECNE.py
@@ -0,0 +1,10 @@
+import glob
+from pathlib import Path
+import subprocess
+
+for r1csFile in glob.glob('generated/**/*.r1cs', recursive=True):
+    #print(r1csFile)
+    rFilePath = Path(r1csFile)
+    rFileWithoutExtensionJustName = rFilePath.with_suffix('').name
+    rFileWithSymExtension = rFilePath.with_suffix('.sym')
+    subprocess.run(["julia", "--project=../ECNEProject/.", "../ECNEProject/src/Ecne.jl", "--name", rFileWithoutExtensionJustName ,"--r1cs", rFilePath, "--sym", rFileWithSymExtension])
