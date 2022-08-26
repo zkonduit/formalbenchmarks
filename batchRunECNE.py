d742138fc6fb649f74cb3463d4adfcf5a14fa7bd
Michael <mchu4545@gmail.com>
2022-08-26 13:49:03 -0700

    only running ecne for O0

--- batchRunECNE.py
+++ batchRunECNE.py
@@ -2,7 +2,7 @@ import glob
 from pathlib import Path
 import subprocess
 
-for r1csFile in glob.glob('generated/**/*.r1cs', recursive=True):
+for r1csFile in glob.glob('generated/O0/*.r1cs', recursive=True):
     #print(r1csFile)
     rFilePath = Path(r1csFile)
     rFileWithoutExtensionJustName = rFilePath.with_suffix('').name
