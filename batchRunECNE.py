10e46f5d867a4db5e63216cb389681a9a480ba5e
Michael <mchu4545@gmail.com>
2022-08-26 10:44:53 -0700

    changing case of EcneProject to match github action

--- batchRunECNE.py
+++ batchRunECNE.py
@@ -7,4 +7,4 @@ for r1csFile in glob.glob('generated/**/*.r1cs', recursive=True):
     rFilePath = Path(r1csFile)
     rFileWithoutExtensionJustName = rFilePath.with_suffix('').name
     rFileWithSymExtension = rFilePath.with_suffix('.sym')
-    subprocess.run(["julia", "--project=../ECNEProject/.", "../ECNEProject/src/Ecne.jl", "--name", rFileWithoutExtensionJustName ,"--r1cs", rFilePath, "--sym", rFileWithSymExtension])
+    subprocess.run(["julia", "--project=../EcneProject/.", "../EcneProject/src/Ecne.jl", "--name", rFileWithoutExtensionJustName ,"--r1cs", rFilePath, "--sym", rFileWithSymExtension])
