db3b38c4a8b92145a9a80dd6e40bf02e032af8b2
Michael <mchu4545@gmail.com>
2022-08-29 12:30:04 -0700

    Committed to a different file after talking to Yanju, trying 10s timeout

--- batchRunPicus.py
+++ batchRunPicus.py
@@ -5,6 +5,5 @@ import subprocess
 for r1csFile in glob.glob('generated/O0/*.r1cs', recursive=True):
     #print(r1csFile)
     rFilePath = Path(r1csFile)
-    #rFileWithoutExtensionJustName = rFilePath.with_suffix('').name
     #rFileWithSymExtension = rFilePath.with_suffix('.sym')
-    subprocess.run(["racket", "../Picus/test-inc-uniqueness.rkt","--r1cs", rFilePath])
+    subprocess.run(["racket", "../Picus/test-uniqueness.rkt","--r1cs", rFilePath, "--timeout", "10000"])
