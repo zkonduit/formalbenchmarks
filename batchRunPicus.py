1b3044eb8bfb1412767a72afc4e3ec1adfdab756
Michael <mchu4545@gmail.com>
2022-08-29 15:50:33 -0700

    Going back to running from Picus instead of main branch

--- batchRunPicus.py
+++ batchRunPicus.py
@@ -2,8 +2,8 @@ import glob
 from pathlib import Path
 import subprocess
 
-for r1csFile in glob.glob('generated/O0/*.r1cs', recursive=True):
+for r1csFile in glob.glob('../main/generated/O0/*.r1cs', recursive=True):
     #print(r1csFile)
     rFilePath = Path(r1csFile)
     #rFileWithSymExtension = rFilePath.with_suffix('.sym')
-    subprocess.run(["racket", "../Picus/test-uniqueness.rkt","--r1cs", rFilePath, "--timeout", "10000"])
+    subprocess.run(["racket", "test-uniqueness.rkt","--r1cs", rFilePath, "--timeout", "10000"])
