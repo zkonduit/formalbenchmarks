6993555841a63b0e7cdd6b1a141dfd3508b1ddf1
Michael <mchu4545@gmail.com>
2022-08-30 00:52:51 -0700

    Going back to running from Picus instead of main branch

--- batchRunPicus.py
+++ batchRunPicus.py
@@ -6,4 +6,4 @@ for r1csFile in glob.glob('../main/generated/O0/*.r1cs', recursive=True):
     #print(r1csFile)
     rFilePath = Path(r1csFile)
     #rFileWithSymExtension = rFilePath.with_suffix('.sym')
-    subprocess.run(["racket", "test-uniqueness.rkt","--r1cs", rFilePath, "--timeout", "10000"])
+    subprocess.run(["racket", "test-pp-uniqueness.rkt","--r1cs", rFilePath, "--timeout", "3000"])
