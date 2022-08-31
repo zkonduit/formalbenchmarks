cd0f4e0a6962144d14be670909411b888702c3d4
Michael <mchu4545@gmail.com>
2022-08-31 01:22:51 -0700

    okay! this should work end to end

--- batchRunECNE.py
+++ batchRunECNE.py
@@ -10,22 +10,22 @@ circuitToStatus = {}
 markdownOutput = """\n| Circuit | Tool | Sound Constraints? |
 | -------- | ---- | ---- | """
 
-for r1csFile in glob.glob('generated/O0/*IsEqual*.r1cs', recursive=True):
+for r1csFile in glob.glob('generated/O0/*.r1cs', recursive=True):
     #print(r1csFile)
     rFilePath = Path(r1csFile)
     rFileWithoutExtensionJustName = rFilePath.with_suffix('').name
     rFileWithSymExtension = rFilePath.with_suffix('.sym')
 
-    #output = subprocess.run(["julia", "--project=EcneProject/.", "EcneProject/src/Ecne.jl", "--name", rFileWithoutExtensionJustName ,"--r1cs", rFilePath, "--sym", rFileWithSymExtension],  capture_output=True)
-    subprocess.run(["julia", "--project=EcneProject/.", "EcneProject/src/Ecne.jl", "--name", rFileWithoutExtensionJustName ,"--r1cs", rFilePath, "--sym", rFileWithSymExtension])
+    output = subprocess.run(["julia", "--project=EcneProject/.", "EcneProject/src/Ecne.jl", "--name", rFileWithoutExtensionJustName ,"--r1cs", rFilePath, "--sym", rFileWithSymExtension],  capture_output=True)
+    #subprocess.run(["julia", "--project=EcneProject/.", "EcneProject/src/Ecne.jl", "--name", rFileWithoutExtensionJustName ,"--r1cs", rFilePath, "--sym", rFileWithSymExtension])
     # search for a string in the output
-    #if output.stdout.decode('utf-8').find('has sound constraints') != -1:
+    if output.stdout.decode('utf-8').find('has sound constraints') != -1:
         #put this in the map
-    #    circuitToStatus[rFileWithoutExtensionJustName] = 'sound constraints'
-    #    markdownOutput+=('\n| '+ rFileWithoutExtensionJustName + ' | ECNE | :white_check_mark: |')
-    #else:
-    #    circuitToStatus[rFileWithoutExtensionJustName] = 'potentially unsound constraints'
-    #    markdownOutput+=('\n| '+ rFileWithoutExtensionJustName + ' | ECNE | :x: |')
+        circuitToStatus[rFileWithoutExtensionJustName] = 'sound constraints'
+        markdownOutput+=('\n| '+ rFileWithoutExtensionJustName + ' | ECNE | :white_check_mark: |')
+    else:
+        circuitToStatus[rFileWithoutExtensionJustName] = 'potentially unsound constraints'
+        markdownOutput+=('\n| '+ rFileWithoutExtensionJustName + ' | ECNE | :x: |')
 
 # use Counter to count the number of each status
 statusCount = Counter(circuitToStatus.values())
