aa7d54ec17e707a37be9b14f86b50c66b9b0f99e
Michael <mchu4545@gmail.com>
2022-08-31 01:04:34 -0700

    debugging just one circuit

--- batchRunECNE.py
+++ batchRunECNE.py
@@ -16,15 +16,16 @@ for r1csFile in glob.glob('generated/O0/*IsEqual*.r1cs', recursive=True):
     rFileWithoutExtensionJustName = rFilePath.with_suffix('').name
     rFileWithSymExtension = rFilePath.with_suffix('.sym')
 
-    output = subprocess.run(["julia", "--project=EcneProject/.", "EcneProject/src/Ecne.jl", "--name", rFileWithoutExtensionJustName ,"--r1cs", rFilePath, "--sym", rFileWithSymExtension],  capture_output=True)
+    #output = subprocess.run(["julia", "--project=EcneProject/.", "EcneProject/src/Ecne.jl", "--name", rFileWithoutExtensionJustName ,"--r1cs", rFilePath, "--sym", rFileWithSymExtension],  capture_output=True)
+    subprocess.run(["julia", "--project=EcneProject/.", "EcneProject/src/Ecne.jl", "--name", rFileWithoutExtensionJustName ,"--r1cs", rFilePath, "--sym", rFileWithSymExtension])
     # search for a string in the output
-    if output.stdout.decode('utf-8').find('has sound constraints') != -1:
+    #if output.stdout.decode('utf-8').find('has sound constraints') != -1:
         #put this in the map
-        circuitToStatus[rFileWithoutExtensionJustName] = 'sound constraints'
-        markdownOutput+=('\n| '+ rFileWithoutExtensionJustName + ' | ECNE | :white_check_mark: |')
-    else:
-        circuitToStatus[rFileWithoutExtensionJustName] = 'potentially unsound constraints'
-        markdownOutput+=('\n| '+ rFileWithoutExtensionJustName + ' | ECNE | :x: |')
+    #    circuitToStatus[rFileWithoutExtensionJustName] = 'sound constraints'
+    #    markdownOutput+=('\n| '+ rFileWithoutExtensionJustName + ' | ECNE | :white_check_mark: |')
+    #else:
+    #    circuitToStatus[rFileWithoutExtensionJustName] = 'potentially unsound constraints'
+    #    markdownOutput+=('\n| '+ rFileWithoutExtensionJustName + ' | ECNE | :x: |')
 
 # use Counter to count the number of each status
 statusCount = Counter(circuitToStatus.values())
