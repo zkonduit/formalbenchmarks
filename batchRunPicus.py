d2eb3c2c7327894e847a4b74d6f724d622c1480a
Michael <mchu4545@gmail.com>
2022-09-02 01:07:55 -0700

    fixing path

--- batchRunPicus.py
+++ batchRunPicus.py
@@ -5,7 +5,7 @@ import subprocess
 markdownOutput = """\n| Circuit | Tool | Sound Constraints? |
 | -------- | ---- | ---- | """
 
-for r1csFile in glob.glob('../main/generated/O0/*.r1cs', recursive=True): # PROD 
+for r1csFile in glob.glob('generated/O0/*.r1cs', recursive=True): # PROD 
 # for r1csFile in glob.glob('../formalbenchmarks/generated/O0/*.r1cs', recursive=True): # DEV
 #for r1csFile in glob.glob('benchmarks/circomlib/*.r1cs', recursive=True): # DEV
     #print(r1csFile)
