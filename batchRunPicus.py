0bfb2a5cf1e12a67f47284e7e0cbb0b99698f3e6
Michael <mchu4545@gmail.com>
2022-09-09 21:57:58 -0700

    increasing timeout for picus

--- batchRunPicus.py
+++ batchRunPicus.py
@@ -15,16 +15,16 @@ for r1csFile in glob.glob('../formalbenchmarks/generated/O0/*.r1cs', recursive=T
     #print (r1csFile)
     #rFileWithSymExtension = rFilePath.with_suffix('.sym')
     try:
-        output = subprocess.run(["racket", "Picus/test-v3-uniqueness.rkt","--r1cs", r1csFile, "--weak", "--timeout", "3000"], timeout=600, capture_output=True) # prod
+        output = subprocess.run(["racket", "Picus/test-v3-uniqueness.rkt","--r1cs", r1csFile, "--weak", "--timeout", "5000"], timeout=700, capture_output=True) # prod
        # output = subprocess.run(["racket", "test-pp-uniqueness.rkt","--r1cs", r1csFile, "--weak", "--timeout", "5000"], timeout=600, capture_output=True) # dev
 
         subprocess.run(["racket", "test-v3-uniqueness.rkt","--r1cs", r1csFile, "--weak", "--timeout", "3000"], timeout=600)
         if output.stdout.decode('utf-8').find('safety verified.') != -1:
             #put this in the map
-            print (rFileWithoutExtensionJustName + ' verified')
+            #print (rFileWithoutExtensionJustName + ' verified')
             markdownOutput+=('\n| '+ rFileWithoutExtensionJustName + ' | Picus | :white_check_mark: |')
         else:
-            print (rFileWithoutExtensionJustName + ' not verified')
+            #print (rFileWithoutExtensionJustName + ' not verified')
 
             markdownOutput+=('\n| '+ rFileWithoutExtensionJustName + ' | Picus | :x: |')
     
