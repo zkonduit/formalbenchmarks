84a8fd682484d2f7cb415c141ba2949c881b5453
Michael <mchu4545@gmail.com>
2022-09-23 15:59:38 -0700

    trying with cvc5??

---
+++ batchRunPicusV3CVC5.py
@@ -0,0 +1,42 @@
+import glob
+from pathlib import Path
+import subprocess
+
+#markdownOutput = """\n| Circuit | Tool | Sound Constraints? |
+#| -------- | ---- | ---- | """
+
+# instantiate dictionary
+picusCircuitToStatus = {}
+
+for r1csFile in glob.glob('generated/O0/*.r1cs', recursive=True): # PROD 
+#for r1csFile in glob.glob('../formalbenchmarks/generated/O0/*.r1cs', recursive=True): # DEV
+#for r1csFile in glob.glob('benchmarks/circomlib/*.r1cs', recursive=True): # DEV
+    #print("picus", r1csFile)
+    rFilePath = Path(r1csFile)
+    rFileWithoutExtensionJustName = rFilePath.with_suffix('').name
+    #print (rFilePath)
+    #print (r1csFile)
+    #rFileWithSymExtension = rFilePath.with_suffix('.sym')
+    try:
+        output = subprocess.run(["racket", "Picus/test-v3-uniqueness.rkt","--r1cs", r1csFile, "--weak", "--solver", "cvc5", "--timeout", "10000"], timeout=1000, capture_output=True) # prod
+       # output = subprocess.run(["racket", "test-pp-uniqueness.rkt","--r1cs", r1csFile, "--weak", "--timeout", "5000"], timeout=600, capture_output=True) # dev
+
+       # subprocess.run(["racket", "test-v3-uniqueness.rkt","--r1cs", r1csFile, "--weak", "--timeout", "3000"], timeout=600)
+        if output.stdout.decode('utf-8').find('safety verified.') != -1:
+            #put this in the map
+            picusCircuitToStatus[rFileWithoutExtensionJustName] = { "tool" : "Picus v3/cvc5", "result" : "Weakly Verified"}
+            #print (rFileWithoutExtensionJustName + ' verified')
+            #markdownOutput+=('\n| '+ rFileWithoutExtensionJustName + ' | Picus | :white_check_mark: |')
+        else:
+            #print (rFileWithoutExtensionJustName + ' not verified')
+            #put this in the map
+            picusCircuitToStatus[rFileWithoutExtensionJustName] = { "tool" : "Picus v3/cvc5", "result" : "Not Verified"}
+            #markdownOutput+=('\n| '+ rFileWithoutExtensionJustName + ' | Picus | :x: |')
+    
+    except subprocess.TimeoutExpired:
+         #print("Timeout!!!!!!!!!", rFileWithoutExtensionJustName)
+         #put this in the map
+         picusCircuitToStatus[rFileWithoutExtensionJustName] = { "tool" : "Picus v3/cvc5", "result" : "Timeout"}
+         #markdownOutput+=('\n| '+ rFileWithoutExtensionJustName + ' | Picus | :alarm_clock: |')
+#print(markdownOutput)
+print (picusCircuitToStatus)
